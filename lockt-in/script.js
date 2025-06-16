aa62b4b0482c7d08a21e0a2c60db9647c62dadb8
ASPCoding <152766272+ASPCoding@users.noreply.github.com>
2025-06-15 19:27:45 -0700

    Add changes to id names

--- lockt-in/script.js
+++ lockt-in/script.js
@@ -1,3 +1,36 @@
 function listenForClicks() {
   document.addEventListener("click", (e) => {});
 }
+
+var start = document.getElementById("start");
+var pause = document.getElementById("pause");
+var t_a_break = document.getElementById("break");
+var timer = document.getElementById("time");
+
+var time = "25:00";
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  time = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    time = "EXPIRED";
+  }
+}, 1000);
